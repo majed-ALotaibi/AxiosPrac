c42745271b2c61c8f776b49f1799c6dac9acdda1
majed <al8iisr@gmail.com>
2021-07-20 00:46:03 +0300

    jokes

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-    .then(res=>{    
-        console.log(res.data.ticker.price);
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res=>{    
+//         console.log(res.data.ticker.price);
 
-    })
-    .catch(err=>{
-        console.log(err);
-    })
-    
+//     })
+//     .catch(err=>{
+//         console.log(err);
+//     }
+
+const btn = document.querySelector("button");
+const joke = document.querySelector("#jokes")
+
+const dadsJokes = async () => {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+}
+let newDadjoke =  async() => {
+    let dadJoke =await dadsJokes();
+    let newLI = document.createElement("LI");
+    newLI.append(dadJoke);
+    joke.append(newLI);
+}
+btn.addEventListener("click", newDadjoke);
+
